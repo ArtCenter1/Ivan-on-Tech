d559f5981a0586be0d7f7a89942907373459730c
ArtCenter1 <artcenter1@gmail.com>
2020-08-15 22:27:29 +0800

    types and arrays

--- EthereumProgramming/types-and-arrays.sol
+++ EthereumProgramming/types-and-arrays.sol
@@ -2,7 +2,7 @@ pragma solidity 0.5.12;
 
 contract TypesArrays{
 //state variables
-    string public message = "Hello World";
+    string public message = "Types and Arrays";
     uint[] public numbers = [1, 20, 45];
 //Functions
     function getMessage() public view returns(string memory){
@@ -10,5 +10,11 @@ contract TypesArrays{
     }
     function setMessage(string memory newMessage) public {
       message = newMessage;
+    }
+    function getNumber(uint index) public view returns(uint){
+      return numbers[index];
+    }
+    function setNumber(uint index,uint newNumber) public{
+      numbers[index] = newNumber;
     }
 }
