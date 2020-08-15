f17b27e771060c88c053e55d50b2ad4de37d7004
ArtCenter1 <artcenter1@gmail.com>
2020-08-15 11:31:52 +0800

    start

--- EthereumProgramming/Mapping.sol
+++ EthereumProgramming/Mapping.sol
@@ -1,17 +1,15 @@
 pragma solidity 0.5.12;
-<<<<<<< HEAD
 
 contract Mapping{
 //state variables
-  String public Message = "Hello World";
-//Functions
-=======
-contract HelloWorld{
-  //state variables
-  String public Message = "Hello World";
-  //Functions
->>>>>>> f0d2b18645858d78f58d5ac61bbad45e60157842
-  function setMessage(string memory newMessage) public {
-    Message = newMessage;
+struct Person {
+    uint id;
+    string name;
+    uint age;
+    uint height;
   }
+
+//Functions
+
+
 }
