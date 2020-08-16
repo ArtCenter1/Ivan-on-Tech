31682eef0393f473dc9e6b3dd9610743d9adf3aa
ArtCenter1 <artcenter1@gmail.com>
2020-08-17 06:00:27 +0800

    mod

---
+++ EthereumProgramming/struts.sol
@@ -0,0 +1,21 @@
+pragma solidity 0.5.12;
+
+contract Struts{
+//state variables
+    struct Person {
+      uint id;
+      string name;
+      uint age;
+      uint height;
+    }
+    Person[] public people;
+//Functions
+    function createPerson(string memory name,uint age,uint height) public{
+    //people.push( Person(people.length, name, age, height));
+      Person memory newPerson;
+      newPerson.id = people.length;
+      newPerson.name = name;
+      newPerson.age = age;
+      newPerson.hight = hight;
+      people.push(newPerson);
+}
