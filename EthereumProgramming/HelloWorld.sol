69b70dede8748fee9046cb4d586c99d6990d944e
ArtCenter1 <artcenter1@gmail.com>
2020-08-16 14:34:35 +0800

    add

--- EthereumProgramming/HelloWorld.sol
+++ EthereumProgramming/HelloWorld.sol
@@ -3,6 +3,9 @@ contract HelloWorld{
   //state variables
   String public Message = "Hello World";
   //Functions
+  function getMessage() public view returns(string memory){
+    return Message;
+  }
   function setMessage(string memory newMessage) public {
     Message = newMessage;
   }
