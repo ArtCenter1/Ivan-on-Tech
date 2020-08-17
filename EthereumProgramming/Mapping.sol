82c91da5ff94e41de477e18b97641e29796ef243
ArtCenter1 <artcenter1@gmail.com>
2020-08-17 12:16:50 +0800

    update mapping.sol

--- EthereumProgramming/Mapping.sol
+++ EthereumProgramming/Mapping.sol
@@ -2,14 +2,24 @@ pragma solidity 0.5.12;
 
 contract Mapping{
 //state variables
-struct Person {
-    uint id;
+  struct Person {
     string name;
     uint age;
     uint height;
   }
-
+  mapping(address=> Person) private people;
 //Functions
-
-
+  function createPerson(string memory name,uint age,uint height) public{
+  address creator = msg.sender;
+  //This creates a person
+  Person memory newPerson;
+  newPerson.name = name;
+  newPerson.age = age;
+  newPerson.height = height;
+  people[creator] = newPerson;
+  }
+  function getPerson() public view returns(string memory name,uint age,uint height){
+      address creator = msg.sender;
+      return (people[creator].name,people[creator].age,people[creator].height);
+  }
 }
