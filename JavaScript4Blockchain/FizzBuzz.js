d3cf3310460342120e4c98d255e87d0e45fe632f
ArtCenter1 <artcenter1@gmail.com>
2020-08-23 09:23:57 +0800

    typo correction

--- JavaScript4Blockchain/FizzBuzz.js
+++ JavaScript4Blockchain/FizzBuzz.js
@@ -2,9 +2,9 @@
 
 for (var i = 1; i < 100; i++) {
   if (i%3=0)&&(i%5=0) {
-    console.log("FuzzBuzz")
+    console.log("FizzBuzz")
   }else if (i%3=0) {
-    console.log("Fuzz")
+    console.log("Fizz")
   }else if (i%5=0) {
     console.log("Buzz")
   }
