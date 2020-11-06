cab0bff86094a5f62b9217994dc3ac8952f211b8
ArtCenter1 <artcenter1@gmail.com>
2020-11-06 14:18:28 +0800

    update

--- EthereumProgramming/Require.sol
+++ EthereumProgramming/Require.sol
@@ -8,29 +8,45 @@ contract Require{
     uint height
     bool senior;
   }
+  address public owner;
+  constructor() public {
+    owner = msg.sender;
+  }
   mapping(address=> Person) private people;
+  address[] private creators;
 //Functions
   function createPerson(string memory name,uint age,uint height) public{
-  address creator = msg.sender;
+    require(age < 150, "Age needs to be below 150");
   //This creates a person
-  Person memory newPerson;
-  newPerson.name = name;
-  newPerson.age = age;
-  newPerson.height = height;
-  if(age >= 65){
-          newPerson.senior = true;
-      }
+    Person memory newPerson;
+    newPerson.name = name;
+    newPerson.age = age;
+    newPerson.height = height;
+    if(age >=65){
+      newPerson.senior = true;
+    }
       else{
-          newPerson.senior = false;
-      }
+      newPerson.senior = false;
+    }
+    insertPerson(newPerson);
+    creators.push(msg.sender);
+    }
 
-       insertPerson(newPerson);
-       creators.push(msg.sender);
-   }
-  people[creator] = newPerson;
+  function insertPerson(Person memory newPerson) private{
+    address creator = msg.sender;
+    people[creator] = newPerson;
   }
-  function getPerson() public view returns(string memory name,uint age,uint height){
+
+  function getPerson() public view returns(string memory name,uint age,uint height,bool senior){
       address creator = msg.sender;
-      return (people[creator].name,people[creator].age,people[creator].height);
+      return (people[creator].name,people[creator].age,people[creator].height,people[creator].senior);
   }
-}
+
+  function deletePerson(address creator) public {
+      require(msg.sender == owner);
+      delete people[creator];
+  }
+
+  function getCreator(uint index) public view returns(address){
+      require(msg.sender == owner, "Caller needs to be owner");
+      return creators[index];
