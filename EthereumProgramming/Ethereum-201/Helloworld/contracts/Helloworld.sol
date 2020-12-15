6d2624fe505c1c4ab26c774cd64ddb3a64106702
ArtCenter1 <artceter1@gmail.com>
2020-12-15 17:28:02 +0800

    Helloworld unit test 2

--- EthereumProgramming/Ethereum-201/Helloworld/contracts/Helloworld.sol
+++ EthereumProgramming/Ethereum-201/Helloworld/contracts/Helloworld.sol
@@ -4,19 +4,11 @@ pragma solidity 0.5.12;
 contract Helloworld {
  string message = "Hello World!";
 
-//<<<<<<< HEAD
-  // function getMessage() public view returns (string memory) {
-  //   return message;
-  // }
-  // function setMessage(string memory newMessage) public payable {
-  //   message = newMessage;
-  // }
-// =======
  function getMessage() public view returns (string memory) {
    return message;
  }
  function setMessage(string memory newMessage) public payable {
-  require(msg.value >= 1 ether);
+  //require(msg.value >= 1 ether);
   message = newMessage;
  }
 // >>>>>>> 0854fd89f704021d85b0bdf432c01fa55a7aad06
