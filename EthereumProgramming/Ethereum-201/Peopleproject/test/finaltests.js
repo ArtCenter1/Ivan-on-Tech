33741c5b6e9bd8650db8174b019477a89b9fef58
ArtCenter1 <artceter1@gmail.com>
2020-12-18 12:44:28 +0800

    test update

--- EthereumProgramming/Ethereum-201/Peopleproject/test/finaltests.js
+++ EthereumProgramming/Ethereum-201/Peopleproject/test/finaltests.js
@@ -12,17 +12,17 @@ contract("People", async function(accounts){
   it("shouldn't create a person with age over 150 years", async function(){
     await truffleAssert.fails(instance.createPerson("Bob", 200, 190, {value: web3.utils.toWei("1", "ether")}), truffleAssert.ErrorType.REVERT);
   });
-  // it("shouldn't create a person without payment", async function(){
-  //   await truffleAssert.fails(instance.createPerson("Bob", 50, 190, {value: 1000}), truffleAssert.ErrorType.REVERT);
-  // });
-  // it("should set senior status correctly", async function(){
-  //   await instance.createPerson("Bob", 65, 190, {value: web3.utils.toWei("1", "ether")});
-  //   let result = await instance.getPerson();
-  //   assert(result.senior === true, "Senior level not set");
-  // });
-  // it("should set age correctly", async function(){
-  //   let result = await instance.getPerson();
-  //   assert(result.age.toNumber() === 65, "Age not set correctly");
+  it("shouldn't create a person without payment", async function(){
+    await truffleAssert.fails(instance.createPerson("Bob", 50, 190, {value: 1000}), truffleAssert.ErrorType.REVERT);
+  });
+  it("should set senior status correctly", async function(){
+    await instance.createPerson("Bob", 65, 190, {value: web3.utils.toWei("1", "ether")});
+    let result = await instance.getPerson();
+    assert(result.senior === true, "Senior level not set");
+  });
+  it("should set age correctly", async function(){
+    let result = await instance.getPerson();
+    assert(result.age.toNumber() === 65, "Age not set correctly");
   // });
   // it("should not allow non-owner to delete people", async function(){
   //   let instance = await People.deployed();
